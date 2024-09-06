102073dcbe140a5e650bdc0c280a2270b8ecb264
minomino <minodatomomi@minodatomominoMacBook-Pro.local>
2024-09-06 18:43:20 +0900

    CSS調整

--- src/components/addDialog.tsx
+++ src/components/addDialog.tsx
@@ -78,166 +78,129 @@ export default function AddDialog(props: {
   };
 
   return (
-    <div className="bg-black-rgba fixed  inset-0 z-20 flex items-center justify-center">
-      <div className="relative w-full p-4">
-        <div className="relative h-[75vh] max-w-[355px] rounded-lg border-2 border-[#090A0A] bg-white pt-3">
-          <div className="mr-2 flex items-center rounded-t md:p-5">
-            <button
-              type="button"
-              className="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900"
-              onClick={() => showModal(false)}
+    <div className="bg-black-rgba fixed inset-0 z-20 flex w-full items-center justify-center p-4">
+      <div className="h-[75vh] w-full max-w-[355px] rounded-lg border-2 border-[#090A0A] bg-white">
+        <div className="mr-2 flex items-center rounded-t md:p-5">
+          <button
+            type="button"
+            className="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900"
+            onClick={() => showModal(false)}
+          >
+            <svg
+              className="h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
             >
-              <svg
-                className="h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="mx-auto h-[88%] overflow-x-hidden">
+          <form className="" onSubmit={handleSubmit(onSubmit)}>
+            <div className="text-center">
+              <label
+                htmlFor="image-upload"
+                className="mb-8 inline-block cursor-pointer"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                />
-              </svg>
-            </button>
-          </div>
-          <div className="mx-auto h-[90%] overflow-x-hidden">
-            <form className="" onSubmit={handleSubmit(onSubmit)}>
-              <div className="text-center">
-                <label
-                  htmlFor="image-upload"
-                  className="mb-8 inline-block cursor-pointer"
-                >
-                  <div className="text-center">
-                    <div className="q flex h-32 w-32 items-center justify-center overflow-hidden rounded-full bg-gray-200">
-                      {imageFile ? (
-                        <div className="relative">
-                          <img
-                            src={URL.createObjectURL(imageFile)}
-                            alt="Selected"
-                            className="h-full w-full object-cover"
-                          />
-                          <div className="absolute left-1/2 top-1/2 h-32 w-32 -translate-x-1/2 -translate-y-1/2 rounded-full bg-black/40"></div>
-                          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
-                            <svg
-                              width="30"
-                              height="27"
-                              viewBox="0 0 30 27"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M15 21.75C16.875 21.75 18.4687 21.0937 19.7812 19.7812C21.0937 18.4687 21.75 16.875 21.75 15C21.75 13.125 21.0937 11.5312 19.7812 10.2187C18.4687 8.90625 16.875 8.25 15 8.25C13.125 8.25 11.5312 8.90625 10.2187 10.2187C8.90625 11.5312 8.25 13.125 8.25 15C8.25 16.875 8.90625 18.4687 10.2187 19.7812C11.5312 21.0937 13.125 21.75 15 21.75ZM15 18.75C13.95 18.75 13.0625 18.3875 12.3375 17.6625C11.6125 16.9375 11.25 16.05 11.25 15C11.25 13.95 11.6125 13.0625 12.3375 12.3375C13.0625 11.6125 13.95 11.25 15 11.25C16.05 11.25 16.9375 11.6125 17.6625 12.3375C18.3875 13.0625 18.75 13.95 18.75 15C18.75 16.05 18.3875 16.9375 17.6625 17.6625C16.9375 18.3875 16.05 18.75 15 18.75ZM3 27C2.175 27 1.46875 26.7062 0.88125 26.1187C0.29375 25.5312 0 24.825 0 24V6C0 5.175 0.29375 4.46875 0.88125 3.88125C1.46875 3.29375 2.175 3 3 3H7.725L10.5 0H19.5L22.275 3H27C27.825 3 28.5312 3.29375 29.1187 3.88125C29.7062 4.46875 30 5.175 30 6V24C30 24.825 29.7062 25.5312 29.1187 26.1187C28.5312 26.7062 27.825 27 27 27H3ZM3 24H27V6H20.925L18.1875 3H11.8125L9.075 6H3V24Z"
-                                fill="white"
-                              />
-                            </svg>
-                          </div>
-                        </div>
-                      ) : (
-                        <svg
-                          width="30"
-                          height="27"
-                          viewBox="0 0 30 27"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M15 21.75C16.875 21.75 18.4687 21.0937 19.7812 19.7812C21.0937 18.4687 21.75 16.875 21.75 15C21.75 13.125 21.0937 11.5312 19.7812 10.2187C18.4687 8.90625 16.875 8.25 15 8.25C13.125 8.25 11.5312 8.90625 10.2187 10.2187C8.90625 11.5312 8.25 13.125 8.25 15C8.25 16.875 8.90625 18.4687 10.2187 19.7812C11.5312 21.0937 13.125 21.75 15 21.75ZM15 18.75C13.95 18.75 13.0625 18.3875 12.3375 17.6625C11.6125 16.9375 11.25 16.05 11.25 15C11.25 13.95 11.6125 13.0625 12.3375 12.3375C13.0625 11.6125 13.95 11.25 15 11.25C16.05 11.25 16.9375 11.6125 17.6625 12.3375C18.3875 13.0625 18.75 13.95 18.75 15C18.75 16.05 18.3875 16.9375 17.6625 17.6625C16.9375 18.3875 16.05 18.75 15 18.75ZM3 27C2.175 27 1.46875 26.7062 0.88125 26.1187C0.29375 25.5312 0 24.825 0 24V6C0 5.175 0.29375 4.46875 0.88125 3.88125C1.46875 3.29375 2.175 3 3 3H7.725L10.5 0H19.5L22.275 3H27C27.825 3 28.5312 3.29375 29.1187 3.88125C29.7062 4.46875 30 5.175 30 6V24C30 24.825 29.7062 25.5312 29.1187 26.1187C28.5312 26.7062 27.825 27 27 27H3ZM3 24H27V6H20.925L18.1875 3H11.8125L9.075 6H3V24Z"
-                            fill="#666666"
-                          />
-                        </svg>
-                      )}
-                    </div>
-                    <input
-                      id="image-upload"
-                      type="file"
-                      accept="image/*"
-                      className="sr-only"
-                      onChange={(e) =>
-                        setImageFile(e.target.files ? e.target.files[0] : null)
-                      }
-                    />
-                  </div>
-                </label>
-              </div>
-              <div className="mx-auto mb-4 w-[70%]">
-                <label className="mb-1 block text-sm font-bold text-gray-700">
-                  店名
-                </label>
-                <input
-                  {...register('shop_name')}
-                  type="text"
-                  placeholder="店名"
-                  className="w-full rounded-lg border-2 border-[#090A0A] py-3 pl-3 text-sm text-black"
-                />
-              </div>
-              <div className="mx-auto mb-4 w-[70%]">
-                <label className="mb-1 block text-sm font-bold text-gray-700">
-                  Instagram ID
-                </label>
-                <input
-                  {...register('instagram_id')}
-                  type="text"
-                  placeholder="Instagram ID"
-                  className="w-full rounded-lg border-2 border-[#090A0A] py-3 pl-3 text-sm text-black"
-                />
-              </div>
-              <div className="mb-4">
-                <div className="mb-4 ml-[15%] w-[76%]">
-                  <label className="mb-1 block text-sm font-bold text-gray-700">
-                    好きなメニュー
-                  </label>
-                  {fields.map((field, index) => (
-                    <div
-                      key={field.id}
-                      className="mb-2 flex items-center gap-1"
-                    >
-                      <input
-                        {...register(`favorite_menus.${index}.value`)}
-                        type="text"
-                        placeholder="好きなメニュー"
-                        className="w-full min-w-[237px] rounded-lg border-2 border-[#090A0A] py-3 pl-3 text-sm text-black"
-                      />
-                      {index > 0 && (
-                        <button
-                          type="button"
-                          className="text-red-500"
-                          onClick={() => remove(index)}
-                        >
+                <div className="text-center">
+                  <div className="q flex h-32 w-32 items-center justify-center overflow-hidden rounded-full bg-gray-200">
+                    {imageFile ? (
+                      <div className="relative">
+                        <img
+                          src={URL.createObjectURL(imageFile)}
+                          alt="Selected"
+                          className="h-full w-full object-cover"
+                        />
+                        <div className="absolute left-1/2 top-1/2 h-32 w-32 -translate-x-1/2 -translate-y-1/2 rounded-full bg-black/40"></div>
+                        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
                           <svg
-                            width="15"
-                            height="15"
-                            viewBox="0 0 15 15"
+                            width="30"
+                            height="27"
+                            viewBox="0 0 30 27"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
                           >
-                            <circle
-                              cx="7.5"
-                              cy="7.5"
-                              r="7"
-                              fill="white"
-                              stroke="black"
-                            />
-                            <line
-                              x1="5.02518"
-                              y1="5.02513"
-                              x2="9.97493"
-                              y2="9.97487"
-                              stroke="black"
-                            />
                             <path
-                              d="M9.97485 5.02513L5.02511 9.97487"
-                              stroke="black"
+                              d="M15 21.75C16.875 21.75 18.4687 21.0937 19.7812 19.7812C21.0937 18.4687 21.75 16.875 21.75 15C21.75 13.125 21.0937 11.5312 19.7812 10.2187C18.4687 8.90625 16.875 8.25 15 8.25C13.125 8.25 11.5312 8.90625 10.2187 10.2187C8.90625 11.5312 8.25 13.125 8.25 15C8.25 16.875 8.90625 18.4687 10.2187 19.7812C11.5312 21.0937 13.125 21.75 15 21.75ZM15 18.75C13.95 18.75 13.0625 18.3875 12.3375 17.6625C11.6125 16.9375 11.25 16.05 11.25 15C11.25 13.95 11.6125 13.0625 12.3375 12.3375C13.0625 11.6125 13.95 11.25 15 11.25C16.05 11.25 16.9375 11.6125 17.6625 12.3375C18.3875 13.0625 18.75 13.95 18.75 15C18.75 16.05 18.3875 16.9375 17.6625 17.6625C16.9375 18.3875 16.05 18.75 15 18.75ZM3 27C2.175 27 1.46875 26.7062 0.88125 26.1187C0.29375 25.5312 0 24.825 0 24V6C0 5.175 0.29375 4.46875 0.88125 3.88125C1.46875 3.29375 2.175 3 3 3H7.725L10.5 0H19.5L22.275 3H27C27.825 3 28.5312 3.29375 29.1187 3.88125C29.7062 4.46875 30 5.175 30 6V24C30 24.825 29.7062 25.5312 29.1187 26.1187C28.5312 26.7062 27.825 27 27 27H3ZM3 24H27V6H20.925L18.1875 3H11.8125L9.075 6H3V24Z"
+                              fill="white"
                             />
                           </svg>
-                        </button>
-                      )}
+                        </div>
+                      </div>
+                    ) : (
+                      <svg
+                        width="30"
+                        height="27"
+                        viewBox="0 0 30 27"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M15 21.75C16.875 21.75 18.4687 21.0937 19.7812 19.7812C21.0937 18.4687 21.75 16.875 21.75 15C21.75 13.125 21.0937 11.5312 19.7812 10.2187C18.4687 8.90625 16.875 8.25 15 8.25C13.125 8.25 11.5312 8.90625 10.2187 10.2187C8.90625 11.5312 8.25 13.125 8.25 15C8.25 16.875 8.90625 18.4687 10.2187 19.7812C11.5312 21.0937 13.125 21.75 15 21.75ZM15 18.75C13.95 18.75 13.0625 18.3875 12.3375 17.6625C11.6125 16.9375 11.25 16.05 11.25 15C11.25 13.95 11.6125 13.0625 12.3375 12.3375C13.0625 11.6125 13.95 11.25 15 11.25C16.05 11.25 16.9375 11.6125 17.6625 12.3375C18.3875 13.0625 18.75 13.95 18.75 15C18.75 16.05 18.3875 16.9375 17.6625 17.6625C16.9375 18.3875 16.05 18.75 15 18.75ZM3 27C2.175 27 1.46875 26.7062 0.88125 26.1187C0.29375 25.5312 0 24.825 0 24V6C0 5.175 0.29375 4.46875 0.88125 3.88125C1.46875 3.29375 2.175 3 3 3H7.725L10.5 0H19.5L22.275 3H27C27.825 3 28.5312 3.29375 29.1187 3.88125C29.7062 4.46875 30 5.175 30 6V24C30 24.825 29.7062 25.5312 29.1187 26.1187C28.5312 26.7062 27.825 27 27 27H3ZM3 24H27V6H20.925L18.1875 3H11.8125L9.075 6H3V24Z"
+                          fill="#666666"
+                        />
+                      </svg>
+                    )}
+                  </div>
+                  <input
+                    id="image-upload"
+                    type="file"
+                    accept="image/*"
+                    className="sr-only"
+                    onChange={(e) =>
+                      setImageFile(e.target.files ? e.target.files[0] : null)
+                    }
+                  />
+                </div>
+              </label>
+            </div>
+            <div className="mx-auto mb-4 w-[70%]">
+              <label className="mb-1 block text-sm font-bold text-gray-700">
+                店名
+              </label>
+              <input
+                {...register('shop_name')}
+                type="text"
+                placeholder="店名"
+                className="w-full rounded-lg border-2 border-[#090A0A] py-3 pl-3 text-sm text-black"
+              />
+            </div>
+            <div className="mx-auto mb-4 w-[70%]">
+              <label className="mb-1 block text-sm font-bold text-gray-700">
+                Instagram ID
+              </label>
+              <input
+                {...register('instagram_id')}
+                type="text"
+                placeholder="Instagram ID"
+                className="w-full rounded-lg border-2 border-[#090A0A] py-3 pl-3 text-sm text-black"
+              />
+            </div>
+            <div className="mb-4">
+              <div className="mb-4 ml-[15%] w-[76%]">
+                <label className="mb-1 block text-sm font-bold text-gray-700">
+                  好きなメニュー
+                </label>
+                {fields.map((field, index) => (
+                  <div key={field.id} className="mb-2 flex items-center gap-1">
+                    <input
+                      {...register(`favorite_menus.${index}.value`)}
+                      type="text"
+                      placeholder="好きなメニュー"
+                      className="w-full min-w-[237px] rounded-lg border-2 border-[#090A0A] py-3 pl-3 text-sm text-black"
+                    />
+                    {index > 0 && (
                       <button
                         type="button"
-                        className=""
-                        onClick={() => append({ value: '' })}
+                        className="text-red-500"
+                        onClick={() => remove(index)}
                       >
                         <svg
                           width="15"
@@ -254,39 +217,65 @@ export default function AddDialog(props: {
                             stroke="black"
                           />
                           <line
-                            x1="4"
-                            y1="7.5"
-                            x2="11"
-                            y2="7.5"
+                            x1="5.02518"
+                            y1="5.02513"
+                            x2="9.97493"
+                            y2="9.97487"
                             stroke="black"
                           />
-                          <path d="M7.5 4L7.5 11" stroke="black" />
+                          <path
+                            d="M9.97485 5.02513L5.02511 9.97487"
+                            stroke="black"
+                          />
                         </svg>
                       </button>
-                    </div>
-                  ))}
-                </div>
-                <div className="mx-auto mb-4 w-[70%]">
-                  <label className="mb-1 block text-sm font-bold text-gray-700">
-                    好きポイント
-                  </label>
-                  <textarea
-                    {...register('shop_review')}
-                    placeholder="このお店の好きなポイント"
-                    className="h-28 w-full rounded-lg border-2 border-[#090A0A] py-3 pl-3 text-sm text-black"
-                  />
-                </div>
+                    )}
+                    <button
+                      type="button"
+                      className=""
+                      onClick={() => append({ value: '' })}
+                    >
+                      <svg
+                        width="15"
+                        height="15"
+                        viewBox="0 0 15 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <circle
+                          cx="7.5"
+                          cy="7.5"
+                          r="7"
+                          fill="white"
+                          stroke="black"
+                        />
+                        <line x1="4" y1="7.5" x2="11" y2="7.5" stroke="black" />
+                        <path d="M7.5 4L7.5 11" stroke="black" />
+                      </svg>
+                    </button>
+                  </div>
+                ))}
               </div>
               <div className="mx-auto mb-4 w-[70%]">
-                <button
-                  type="submit"
-                  className="w-full rounded-lg border-2 border-[#090A0A] bg-[#FDFF89] py-3 text-sm font-bold text-black"
-                >
-                  登録する
-                </button>
+                <label className="mb-1 block text-sm font-bold text-gray-700">
+                  好きポイント
+                </label>
+                <textarea
+                  {...register('shop_review')}
+                  placeholder="このお店の好きなポイント"
+                  className="h-28 w-full rounded-lg border-2 border-[#090A0A] py-3 pl-3 text-sm text-black"
+                />
               </div>
-            </form>
-          </div>
+            </div>
+            <div className="mx-auto mb-4 w-[70%]">
+              <button
+                type="submit"
+                className="w-full rounded-lg border-2 border-[#090A0A] bg-[#FDFF89] py-3 text-sm font-bold text-black"
+              >
+                登録する
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
