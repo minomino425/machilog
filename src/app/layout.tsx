567f3472abddbee2a4623976ce566e7da4eaf7a0
minomino <minodatomomi@minodatomominoMacBook-Pro.local>
2024-09-09 18:36:26 +0900

    faviconとog image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './globals.css';
 import { Noto_Sans_JP } from 'next/font/google';
+import ogImage from '../../public/og-image.png';
 
 const NotoSansJPFont = Noto_Sans_JP({
   weight: ['400', '500', '600', '700'],
@@ -31,10 +32,7 @@ export default function RootLayout({
         />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://machilog.vercel.app/" />
-        <meta
-          property="og:image"
-          content="https://machilog.vercel.app/og-image.png"
-        />
+        <meta property="og:image" content={ogImage.src} />
         <meta
           property="og:site_name"
           content="🌈マチログ | 町田在住酒好きエンジニアによる外食備忘録🍺"
